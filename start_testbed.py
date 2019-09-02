9aae598badd70374687330746bcb5c77f0377f77
munhyunsu <munhyunsu@gmail.com>
2019-09-02 23:19:44 +0900

    Testbed

--- start_testbed.py
+++ start_testbed.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
 
-class AppSpeedXrayTestBed(Resource):
+class AppSpeedXrayTestbed(Resource):
     def post(self):
         pass
 
@@ -11,4 +11,6 @@ def main():
     app = Flask(__name__)
     api = Api(app)
 
-    api.add_resource(App)
+    api.add_resource(AppSpeedXrayTestbed, '/')
+
+
