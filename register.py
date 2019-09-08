81525957750b42ea700cbb08bfc8cc78a3cbee26
munhyunsu <munhyunsu@gmail.com>
2019-09-08 22:12:43 +0900

    Regist interface

---
+++ register.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from fkask_restful import Resource, Api, reqparse
+
+
+class Registration(Resource):
+    def post(self):
+        # resist testbed client
+        pass
+
+    def get(self):
+        # get information about testbed client
+        # resisted or not
+        pass
+        
