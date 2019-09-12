f5e440e955ceb82b504081715381bc71a624f89a
munhyunsu <munhyunsu@gmail.com>
2019-09-12 12:54:16 +0900

    flask app

--- start_testbed.py
+++ start_testbed.py
@@ -5,11 +5,9 @@ from deliver import Deliver
 from register import Registration
 
 
-def main():
-    app = Flask(__name__)
-    api = Api(app)
+app = Flask(__name__)
+api = Api(app)
 
-    api.add_resource(Deliver, '/')
-    api.add_resource(Registration, '/res')
-
+api.add_resource(Deliver, '/')
+api.add_resource(Registration, '/res')
 
